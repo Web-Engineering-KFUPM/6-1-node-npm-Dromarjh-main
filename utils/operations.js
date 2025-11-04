2712dbb39ba0487af749bfea18b431669806bad7
Omar Hammad <hammado92@gmail.com>
2025-11-04 10:01:03 +0300

    init

---
+++ utils/operations.js
@@ -0,0 +1,16 @@
+export function add(numbers) {
+  return {/*write code here*/}
+}
+
+export function subtract(numbers) {
+  return {/*write code here*/}
+}
+
+export function multiply(numbers) {
+  return {/*write code here*/}
+}
+
+export function divide(numbers) {
+  return {/*write code here*/}
+}
+
---
+++ utils/parser.js
@@ -0,0 +1,10 @@
+import _ from "lodash";
+
+export function parseNumbers(input) {
+  return {/*write code here*/}
+}
+
+export function isValidOperation(operation) {
+  return {/*write code here*/}
+}
+
